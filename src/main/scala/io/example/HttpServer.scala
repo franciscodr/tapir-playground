a32f0536503830b87421ace21eeac3a4f5c3a62d
franciscodr <francisco.d@47deg.com>
2021-11-02 09:36:56 +0100

    Use withLogger option in http4s server builder

--- src/main/scala/io/example/HttpServer.scala
+++ src/main/scala/io/example/HttpServer.scala
@@ -6,19 +6,22 @@ import io.example.algebra.UserRepository
 import io.example.http.Routes
 import io.example.service.{GreetingService, UserService}
 import org.http4s.ember.server.EmberServerBuilder
-import org.http4s.server.middleware.Logger
+import org.typelevel.log4cats.slf4j.Slf4jLogger
 
 object HttpServer extends IOApp.Simple {
   override def run: IO[Unit] = {
     implicit val userRepository: UserRepository[IO] = UserRepository.impl[IO]
     val routes = new Routes[IO](new GreetingService[IO], new UserService[IO])
-    val httpApp = Logger.httpApp[IO](logHeaders = true, logBody = true)(routes.routes.orNotFound)
-    EmberServerBuilder
-      .default[IO]
-      .withHost(host"localhost")
-      .withPort(port"8081")
-      .withHttpApp(httpApp)
-      .build
-      .use(_ => IO.never)
+    Slf4jLogger
+      .create[IO].flatMap(logger =>
+        EmberServerBuilder
+          .default[IO]
+          .withLogger(logger)
+          .withHost(host"localhost")
+          .withPort(port"8081")
+          .withHttpApp(routes.routes.orNotFound)
+          .build
+          .use(_ => IO.never)
+      )
   }
 }
